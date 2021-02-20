01caa517d217774768347f99a7fbcccb49c6811f
tao.wu <tao.wu@zenjoy.net>
2021-02-20 08:39:24 +0800

    updta

--- tools/server.sh
+++ tools/server.sh
@@ -3,12 +3,12 @@ echo "start server ......"
 
 cd ../stock_tool
 
-pid=$(ps -ef | grep "python" | grep -v grep | awk '{print $2}')
+# pid=$(ps -ef | grep "python" | grep -v grep | awk '{print $2}')
 
-if ps -p $pid > /dev/null
-then
-   echo "$pid python is running"
-   kill $pid
-fi
+# if ps -p $pid > /dev/null
+# then
+#    echo "$pid python is running"
+#    kill $pid
+# fi
 
 python -m SimpleHTTPServer
