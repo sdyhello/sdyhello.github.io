46853d606b92d20c04d18835fd626e02e08d3173
tao.wu <tao.wu@zenjoy.net>
2021-08-19 22:34:55 +0800

    update loading

--- tools/stockProject.py
+++ tools/stockProject.py
@@ -10,6 +10,16 @@ print(curPath)
 leftFilePath = "/stock_tool/index.html"
 rightFilePath = "/web-mobile/index.html"
 
+leftCssFile = "/stock_tool/style-mobile.css"
+rightCssFile = "/web-mobile/style-mobile.css"
+
+leftPng = "/stock_tool/splash.png"
+rightPng = "/web-mobile/splash.png"
+
 shutil.copy(curPath + leftFilePath, curPath + rightFilePath)
+shutil.copy(curPath + leftCssFile, curPath + rightCssFile)
+shutil.copy(curPath + leftPng, curPath + rightPng)
+
+
 shutil.rmtree(curPath + "/stock_tool", ignore_errors=True)
 os.rename(curPath + "/web-mobile", curPath + "/stock_tool")
