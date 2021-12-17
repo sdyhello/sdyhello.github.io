348d872db0bbfbba508b91f47e491c3df5f94a38
tao.wu <tao.wu@zenjoy.net>
2021-12-17 18:06:09 +0800

    update

--- game_1_50/main.ca140.js
+++ game_1_50/main.ca140.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.40cef.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.9a045.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
